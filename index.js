5565702f7fe69027eef3ff2682adc0d129c6c65a
gabriel <gabriel.m.ledesma96@gmail.com>
2023-08-02 18:59:20 -0300

    todos los permisos

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 8080
 app.use(express.json());
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://cantinero.vercel.app/#/cart');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
