40a80d4e751c3a3899e12bebfa0e09f614d55409
gabriel <gabriel.m.ledesma96@gmail.com>
2023-08-02 17:56:31 -0300

    links

--- server/index.js
+++ server/index.js
@@ -29,9 +29,9 @@ app.post("/create_preference", (req, res) => {
     items:items,
     
     back_urls: {
-      success: "http://localhost:5173/success", 
-      failure: "http://localhost:5173/failure", 
-      pending: "http://localhost:5173/pending", 
+      success: "https://cantinero.vercel.app/success", 
+      failure: "https://cantinero.vercel.app/failure", 
+      pending: "https://cantinero.vercel.app/pending", 
     },
     auto_return: "approved",
   };
